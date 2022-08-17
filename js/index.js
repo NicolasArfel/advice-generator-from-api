76500a460d327ad4ea2e5adeeaeb2cb0cb324ead
NicicolasArfel <nicolasarfel@gmail.com>
2022-08-17 19:36:09 +0200

    fetch ready, DOM ready, style desktop ok

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// api url
+const api_url = 
+      "https://api.adviceslip.com/advice";
+  
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+
+    const title = document.querySelector('.container__title');
+    const advice = document.querySelector('.container__advice');
+    if(response) {
+        title.innerHTML = "Advice #" + data.slip.id;
+        advice.innerHTML = '“' + data.slip.advice + '”'
+    }
+
+}
+// Calling that async function
+getapi(api_url);
+  
